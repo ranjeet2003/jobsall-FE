495263739d25ed996ec01e178b3b134aaccff67b
Ranjeet Gautam <ranjeet.gautam@indiamart.com>
2023-01-03 19:09:23 +0530

    new svg logo

--- pages/post/internship/[postId].js
+++ pages/post/internship/[postId].js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import { Container } from "@mui/material";
-
+import FUT from '../../../components/follow-telegram'
 const PostsPage = () => {
   const [post, setPost] = useState({});
   const router = useRouter();
@@ -63,6 +63,7 @@ const PostsPage = () => {
           </tr>
         </tbody>
       </table>
+      <FUT />
     </Container>
   );
 };
